0d10701d5eb50302e846b8567815ac746a4946e7
yalqahtani <46534449+yalqahtani@users.noreply.github.com>
2019-03-25 21:18:52 -0600

    Updated View

--- crm/views.py
+++ crm/views.py
@@ -5,6 +5,11 @@ from .forms import *
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
 from django.db.models import Sum
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import CustomerSerializer
+
 
 
 
@@ -167,3 +172,12 @@ def register(request):
     else:
         user_form = UserRegistrationForm()
     return render(request,'crm/register.html',{'user_form': user_form})
+
+# Lists all customers 
+class CustomerList(APIView):
+
+    def get(self,request):
+        customers_json = Customer.objects.all()
+        serializer = CustomerSerializer(customers_json, many=True)
+        return Response(serializer.data)
+
